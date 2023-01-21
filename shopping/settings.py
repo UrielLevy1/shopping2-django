c540ac19d5b1a54d79564c813e53d7ed8b9f412c
UrielLevy1 <your_github_email>
2023-01-21 20:30:34 +0200

    cors aallowed changed

--- shopping/settings.py
+++ shopping/settings.py
@@ -143,6 +143,7 @@ MEDIA_ROOT = BASE_DIR / 'staticfiles/images'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://shopping-site-react.onrender.com",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
